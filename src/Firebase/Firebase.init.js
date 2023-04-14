7c10300079eaa633453c1ee8043b94491d6d19e2
HmSani6699 <shahinur04042002@gmail.com>
2023-04-14 22:19:50 +0600

    first commit

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfw4ruiUQEeI5xoUs6V6Ur6LlHTYwog-4",
+  authDomain: "simple-firebase-41afd.firebaseapp.com",
+  projectId: "simple-firebase-41afd",
+  storageBucket: "simple-firebase-41afd.appspot.com",
+  messagingSenderId: "305141064538",
+  appId: "1:305141064538:web:317d95e857323179cfc963"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/__steps.js
@@ -0,0 +1,6 @@
+//1 visit : console.firebase.google.com
+// 2 create project and (skip google analytic)
+// Register app (create config)
+// install firebase
+// add config file to your project
+// danger :do not public firebase information
